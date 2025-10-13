6704de2fe7888f94f86ad683f6b53035297ac5a1
OBI OBI ETCHU JNR <obi.junior@icloud.com>
2025-10-13 14:42:49 +0100

    Hosting backend

--- src/config/db.js
+++ src/config/db.js
@@ -4,28 +4,15 @@ import dotenv from 'dotenv';
 const { Pool } = pkg;
 dotenv.config();
 
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 5432, 
-//   max: 10,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 2000,
-// });
-
-const connectionString = process.env.DATABASE_URL || 
-  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
-
-  console.log('Database connection string:', 
-  connectionString ? 'Present' : 'Missing'
-);
-
 const pool = new Pool({
-  connectionString: connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432, 
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 // Test connection
